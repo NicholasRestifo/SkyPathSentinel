63b5892da784346697e7ff4cf84b38722614077c
Nicholas Restifo <nick@Nicholass-MacBook-Pro.local>
2026-07-14 23:33:21 -0400

    docs: add TSDoc for countryCode in AirportProperties

--- src/types/models/AirportFeature.ts
+++ src/types/models/AirportFeature.ts
@@ -5,6 +5,7 @@ export interface AirportProperties {
   identifier: string;
   dataSourceType: 'ICAO' | 'FAA' | 'IATA' | 'OTHER';
   name: string;
+  /** ISO 3166-1 alpha-2 country code. */
   countryCode: string;
 }
 export type AirportFeature = SpatialFeature<AirportProperties, PointGeometry>;
